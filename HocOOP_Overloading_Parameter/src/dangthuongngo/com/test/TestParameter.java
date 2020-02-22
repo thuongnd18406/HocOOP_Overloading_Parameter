e7043d05b2d29b59c2d31504cf12f409c2afdd99
thuongnd18406 <58813053+thuongnd18406@users.noreply.github.com>
2020-02-22 16:20:35 +0700

    Add

---
+++ HocOOP_Overloading_Parameter/src/dangthuongngo/com/test/TestParameter.java
@@ -0,0 +1,20 @@
+package dangthuongngo.com.test;
+
+public class TestParameter {
+	public static  int sum(int ... arr)
+	{
+		int s=0;
+		for (int i = 0; i < arr.length; i++) {
+			s+=arr[i];
+		}
+		return s;
+	}
+	public static void main(String[] args) {
+		int s=sum(1,3,4,6,8,4,3);
+		System.out.println(s);
+		int s2=sum();
+		System.out.println(s2);
+		int s3=sum(7,8);
+		System.out.println(s3);
+	}
+}
